790e038acda8d73e982cb472010c165105674d90
Manuel Odendahl <wesen@ruinwesen.com>
2010-09-13 18:34:07 +0200

    adjust paths and relative directories to use asdf: functions load xml catalog (not sure if entirely correct yet)

--- src/config.lisp
+++ src/config.lisp
@@ -3,10 +3,16 @@
 ;; URL für BASE HREFs
 (defparameter *website-url* "http://quickhoney.com")
 
+;; this is kind of a dirty hack to get the root of the thirdparty directory
+(defparameter *thirdparty-directory*
+  (asdf:system-relative-pathname :cl-gd #p"../"))
+
 (defparameter *root-directory* (asdf:system-relative-pathname :quickhoney #p"../"))
 
 (defparameter *store-directory* (merge-pathnames #p"datastore/" *root-directory*))
 
 (defparameter *website-directory* (probe-file (merge-pathnames #p"website/" *root-directory*)))
+
+(defparameter *xml-catalog-directory* (asdf:system-relative-pathname :quickhoney #p"../xml/"))
 
 (defparameter *webserver-port* 8081)
--- src/init.lisp
+++ src/init.lisp
@@ -11,9 +11,18 @@
     (*dumped-image*
      (asdf:oos 'asdf:load-op :quickhoney)
      (format t "; starting from dumped image, skipping store initialization~%"))
+    
     (t
-     (setq cxml::*default-catalog* '("/home/hans/share/xml/catalog"))
+     ;; lookup xml catalog
+     (setq cxml::*default-catalog* (list (namestring (merge-pathnames #p"catalog" quickhoney.config:*xml-catalog-directory*))))
+     (unless (probe-file (first cxml::*default-catalog*))
+       (error "Could not find XML catalog.~%
+Please make sure that ~A points to the correct location, or create
+it if it is missing."
+              (first cxml::*default-catalog*)))
+
      (setf *hunchentoot-default-external-format* (flex:make-external-format :utf-8 :eol-style :lf))
+
      (close-store)
      (make-instance 'store
                     :directory *store-directory*
@@ -27,19 +36,25 @@
      (unless (class-instances 'rss-channel)
        (make-rss-channel "quickhoney" "QuickHoney" "QuickHoney Illustrations" "rss/quickhoney"
                          :items (class-instances 'quickhoney-image)))
+     
      (cl-gd::load-gd-glue)))
+
   (ensure-directories-exist
    (setf tbnl:*tmp-directory* (merge-pathnames "hunchentoot-tmp/" *store-directory*)))
   (actor-start (make-instance 'cron-actor))
   (publish-quickhoney)
+  
   (when (probe-file "site-config.lisp")
     (format t "; loading site configuration file~%")
     (let ((*package* (find-package :quickhoney.config)))
       (load "site-config.lisp")))
+  
   (setf *acceptor* (make-instance 'hunchentoot:acceptor
                                   :port *webserver-port*
                                   :taskmaster (make-instance 'hunchentoot:single-threaded-taskmaster)
                                   :persistent-connections-p nil
                                   :request-dispatcher 'bknr.web:bknr-dispatch))
+
+  ;; XXX store thread to stop later on
   (bt:make-thread (curry #'hunchentoot:start *acceptor*)
                   :name (format nil "HTTP server on port ~A" *webserver-port*)))
