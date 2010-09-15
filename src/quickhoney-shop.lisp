743b778b01701024a537abe56ea285fcf667ca6c
Manuel Odendahl <wesen@ruinwesen.com>
2010-09-15 13:00:38 +0200

    add product persistent class and a few helper functions (not sure if useful yet)

--- src/quickhoney-shop.lisp
+++ src/quickhoney-shop.lisp
@@ -1,7 +1,12 @@
 (in-package :quickhoney)
 
 (define-persistent-class quickhoney-product ()
-  ((image
+  ((price
+    :update
+    :type (or number null)
+    :documentation
+    "Product price in USD")
+   (image
     :update
     :type (or quickhoney-image null)
     :index-type hash-index
@@ -10,3 +15,22 @@
     "Image that this product is related to, or nil for standalone products"))
   (:documentation
    "Mixin class for Quickhoney products, which are usually related to a QUICKHONEY-IMAGE"))
+
+(define-persistent-class quickhoney-pdf-product (blob quickhoney-product)
+  ()
+  (:documentation "Download PDF Quickhoney product. The blob data is the PDF file"))
+
+(defmethod quickhoney-image-pdf-product-p ((image quickhoney-image))
+  (let ((products (products-for-image image)))
+    (not (emptyp (remove-if-not #'(lambda (product)
+                                    (typep product 'quickhoney-pdf-product)) products)))))
+
+(defmethod quickhoney-image-pdf-product ((image quickhoney-image))
+  (let ((products (products-for-image image)))
+    (first (remove-if-not #'(lambda (product)
+                              (typep product 'quickhoney-pdf-product)) products))))
+
+(defmethod quickhoney-image-pdf-price ((image quickhoney-image))
+  (let ((product (quickhoney-image-pdf-product image)))
+    (when product
+      (quickhoney-product-price product))))
