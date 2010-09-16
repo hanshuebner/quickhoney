87620de2aafb555a6dcf9b5878bb1d4147584374
Manuel Odendahl <wesen@ruinwesen.com>
2010-09-16 20:16:25 +0200

    Product information shown as it should be Handle EULA reading and redirect correctly to paypal

--- website/static/pdf_shop.js
+++ website/static/pdf_shop.js
@@ -79,38 +79,82 @@ function shop_hide_form() {
 }
 
 
-function init_shop_overlay(image) {
-    submit_json('/json-paypal?price=' + image.shop_price + '&image=' + image.id,
-		null, // no form
-		partial(make_shop_overlay, image)
-	       );
-}
-
 function show_eula() {
     var eula_window = window.open('/static/eula.html', 'eula_window', "width=700,height=400,status=no,toolbar=no,menubar=no,scrollbars=yes");
     return eula_window;
 }
 
+function format_file_size(size) {
+    return Math.floor(size / 1024) + "kb";
+}
+
+function handle_eula_agreed() {
+    var eula_checkbox = $("eula-checkbox");
+    if (eula_checkbox.checked) {
+    }
+}
+
+function init_shop_overlay(image) {
+    show_cue();
+    submit_json('/json-paypal?price=' + image.shop_price +
+		'&image=' + image.id +
+		'&color=' + pages[current_directory].link_color,
+		null, // no form
+		partial(make_shop_overlay, image)
+	       );
+}
+
+function check_eula_agreed() {
+    var eula_checkbox = $("eula-checkbox");
+    if (!eula_checkbox.checked) {
+	alert("Please agree to the EULA!");
+	return false;
+    } else {
+	return true;
+    }
+}
+
 function make_shop_overlay(image, json) {
+    if (json.queryError) {
+	// XXX error while doing paypal stuff
+	alert("error while paypal: " + json.queryError.errorString);
+	return;
+    }
+    
     var paypalLink = json.queryResult.paypalLink;
+    var buttonLink = json.queryResult.buttonLink;
     log("link " + paypalLink);
     make_overlay('buy-file', 'Buy Art as Vector PDF File', 426,
                  FORM({ action: '#', onsubmit: 'return false' },
 		      DIV({'align': 'center'},
 			   IMG({'src': '/image/' + image.id + '/thumbnail,,160,160'})),
-                      SPACER("Download Artwork ", ARTWORK_NAME(image.name), " for one-time private use only.  ",
+                      SPACER(
+			  "FILE#: " + image.id, BR(),
+			  "Filetype: Vector PDF", BR(),
+			  "Filesize: " + format_file_size(image.shop_size), BR(),
+			  "Price: $" + image.shop_price, BR(), BR(),
+
+			  "Download Artwork ", ARTWORK_NAME(image.name), " for one-time private use only.  ",
                              "Please read our ",
-                             A({ onclick: "show_eula()"},
+                             A({ onclick: "show_eula()", id: 'eula-link'},
 			       "User Agreement"),
                              " and tick the box below to indicate that you agree to be bound to it.",
                              BR(), BR(),
-                             INPUT({ type: 'checkbox', name: 'agree-to-license'}),
+                             INPUT({ id: 'eula-checkbox',
+				     type: 'checkbox',
+				     onclick: 'handle_eula_agreed()',
+				     name: 'agree-to-license'}),
                              " I have read and understood the 'User Agreement' and agree to be bound to the terms set forth in it",
-                             BR(), BR(),
-                             PRICE(image.shop_price + "$"), 
-                             BR()
-//                             buy_product_button(buy_file),
-			    )));
+                          BR(), BR(),
+			  DIV({'align': 'center'},
+			      A({'onclick': 'return check_eula_agreed()',
+				 'target': '_parent',
+				'href': paypalLink },
+				IMG({'src': buttonLink}))),
+			  BR(), BR()
+		      )));
+
+    wait_for_images(hide_cue);
 }
 
 INFOTITLE = partial(SPAN, { 'class': 'notice' });
