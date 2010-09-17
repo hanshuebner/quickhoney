ef3e9aadaa723d0db7aece048b27ed1158348535
Manuel Odendahl <wesen@ruinwesen.com>
2010-09-17 15:23:08 +0200

    Added JSON handler for transaction info PAYPAL buying seems to work

--- src/paypal-handlers.lisp
+++ src/paypal-handlers.lisp
@@ -36,6 +36,16 @@
 	       :initform nil
 	       :documentation "Paypal user information")))
 
+(defmethod paypal-txn-valid-p ((txn paypal-product-transaction))
+  (and (eql (paypal-product-transaction-status txn)
+	    :successful)
+       (not (paypal-txn-expired-p txn))))
+
+(defmethod paypal-txn-expired-p ((txn paypal-product-transaction))
+  (< (+ (paypal-product-transaction-creation-time txn)
+	 (* 3 24 3600))
+      (get-universal-time)))
+
 (defclass json-paypal-checkout-handler (page-handler)
   ())
 
@@ -71,23 +81,19 @@
 			       :token token
 			       :status :ongoing)
 		
-		(json:with-object-element ("queryResult")
-		  (json:with-object ()
-		    (json:encode-object-element
-		     "paypalLink"
-		     link)
-		    (json:encode-object-element
-		     "buttonLink"
-		     (format nil "https://fpdbs.sandbox.paypal.com/dynamicimageweb?cmd=_dynamic-image&pal=~A"
-			     *paypal-secure-merchant-id*)))))))
+		(json:encode-object-element
+		 "paypalLink"
+		 link)
+		(json:encode-object-element
+		 "buttonLink"
+		 (format nil "https://fpdbs.sandbox.paypal.com/dynamicimageweb?cmd=_dynamic-image&pal=~A"
+			 *paypal-secure-merchant-id*)))))
 
 	(error (e)
 	  (format t "error while paypal ~A~%" e)
-	  (json:with-object-element ("queryError")
-	    (json:with-object ()
-	      (json:encode-object-element
-	       "errorString"
-	       (format nil "~A" e)))))))))
+	  (json:encode-object-element
+	   "error"
+	   (format nil "~A" e)))))))
 
 (defclass paypal-success-handler (page-handler)
   ())
@@ -100,8 +106,12 @@
       (error "Could not find paypal transaction for token ~A" token))
     (multiple-value-bind (result response)
 	(cl-paypal:do-express-checkout token)
+      (cl-paypal::unregister-transaction token)
       (make-instance 'paypal-action-log
 		     :result result
+		     :token token)
+      (make-instance 'paypal-action-log
+		     :result response
 		     :token token)
 
       (with-transaction ()
@@ -110,7 +120,7 @@
       
       (setf *paypal-success-res* result
 	    *paypal-success-response* response)
-      (redirect "/"))))
+      (redirect (format nil "/#paypal/~A" token)))))
 
 (defclass paypal-cancel-handler (page-handler)
   ())
@@ -121,14 +131,50 @@
 	 (txn (paypal-transaction-for-token token)))
     (unless txn
       (error "Could not find paypal transaction for token ~A" token))
+
     (let ((response (cl-paypal:get-express-checkout-info token)))
+      (cl-paypal::unregister-transaction token)
       (make-instance 'paypal-action-log
 		     :result response
 		     :token token)
+      
       (with-transaction ()
 	(setf (paypal-product-transaction-creation-time txn) (get-universal-time)
 	      (paypal-product-transaction-status txn) :cancelled))
       
       (setf *paypal-cancel-res* response)
-      (redirect "/"))))
+      (redirect (format nil "/#paypal/~A" token)))))
 
+(defun transaction-with-download-id (id)
+  (paypal-transaction-for-token id))
+
+(defclass quickhoney-client-pdf-handler (object-handler)
+  ()
+  (:default-initargs :query-function #'transaction-with-download-id
+    :object-class 'paypal-product-transaction))
+
+(defmethod handle-object ((handler quickhoney-client-pdf-handler) txn)
+  (cond ((paypal-txn-valid-p txn)
+	 (let ((product (paypal-product-transaction-product txn)))
+	   (handle-if-modified-since (blob-timestamp product))
+	   (setf (header-out :last-modified) (rfc-1123-date (blob-timestamp product)))
+	   (with-http-response (:content-type "application/pdf")
+	     (setf (header-out :content-length) (blob-size product))
+	     (with-open-file (blob-data (blob-pathname product) :element-type '(unsigned-byte 8))
+	       (copy-stream blob-data (send-headers) :element-type '(unsigned-byte 8))))))
+	(t (redirect (format nil "/#paypal/~A" (store-object-id txn))))))
+	
+(defclass json-paypal-transaction-info-handler (object-handler)
+  ()
+  (:default-initargs :query-function #'transaction-with-download-id
+    :object-class 'paypal-product-transaction))
+
+(defmethod handle-object ((handler json-paypal-transaction-info-handler) txn)
+  (with-json-response ()
+    (with-slots (product token status creation-time) txn
+      (json:encode-object-element "token" token)
+      (json:encode-object-element "status" status)
+      (json:encode-object-element "valid" (paypal-txn-valid-p txn))
+      (json:encode-object-element "expired" (paypal-txn-expired-p txn))
+      (json:with-object-element ("image")
+	(image-to-json (quickhoney-product-image product))))))
